66043ed39912e15af43ddd9de49e953224423015
MohamedAwad33 <95631364+MohamedAwad33@users.noreply.github.com>
2026-01-03 05:07:12 +0200

    demonstrate synchronous blocking vs asynchronous timers

---
+++ Day_06_DOM_Timers_and_BOM/Task00_Synchronous_Blocking/script.js
@@ -0,0 +1,30 @@
+// (0).
+
+// Wait 5 Seconds without using SetTimeout
+
+console.log("start")
+
+wait5sec();
+
+fun();
+
+console.log("end");
+
+setTimeout(function () {
+    console.log("timeout after 1 sec.");
+}, 1000)
+
+// ----------------------------------------------------------------- //
+
+function fun() {
+    setTimeout(function () {
+        console.log("timeout immediately.");
+    }, 0)
+}
+
+function wait5sec() {
+    const currentTime = Date.now();
+
+    do { } while (Date.now() < currentTime + 5000);
+    console.log("5 seconds are up!");
+}
