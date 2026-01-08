9c0ad85bfbe2cb2c17e884206501b990ee98a3b3
MohamedAwad33 <95631364+MohamedAwad33@users.noreply.github.com>
2026-01-08 06:24:21 +0200

    disable default context menu using event prevention

---
+++ Day_07_Event_Driven_Programming/Task01_Context_Menu_Prevention/script.js
@@ -0,0 +1,9 @@
+// (1).
+
+// Prevent context menu from being displayed by preventing from mouse right click from its action.
+// Note: context menu is the menu displayed when mouse right button is clicked
+
+addEventListener("contextmenu", function () {
+    event.preventDefault();
+    console.error("Disabled!")
+});
